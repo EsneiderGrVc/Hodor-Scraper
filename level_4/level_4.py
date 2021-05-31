0c19f60a66175876037acad7763a8dba3d81ab72
sneidergv <sneidergv@gmail.com>
2021-05-31 15:45:20 -0500

    level_4 finalized

--- level_4/level_4.py
+++ level_4/level_4.py
@@ -1,26 +1,44 @@
-from lxml.html import fromstring
 import requests
 
-######################FIND PROXIES#########################################
-def get_proxies():
-    url = 'https://free-proxy-list.net/'
-    response = requests.get(url)
-    parser = fromstring(response.text)
-    proxies = set()
-    for i in parser.xpath('//tbody/tr')[:299]:   #299 proxies max
-        proxy = ":".join([i.xpath('.//td[1]/text()')[0],i.xpath('.//td[2]/text()')[0]])
-        proxies.add('http://{}'.format(proxy))
-    return proxies
+URL = 'http://158.69.76.135/level4.php'
+WINDOWS_USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'
 
+header = {
+    'User-Agent': WINDOWS_USER_AGENT,
+    'Referer': URL
+}
+proxy_list = []
 
+# set proxy_list
+with open('./proxy_list.txt', 'r') as proxies:
+    i = 0
+    for line in proxies:
+        proxy_list.append({'http': line[:-1]})
+        # proxy_list['{}'.format(i)] = line[:-1]
+        i += 1
 
-######################write to file in format   IP:PORT######################
-try:
-    proxies = get_proxies()
-    f=open('proxy_list.txt','w')
-    for proxy in proxies:
-        f.write(proxy+'\n')
-    f.close()
-    print ("DONE")
-except:
-    print ("MAJOR ERROR")
+for proxy in range(10):
+    try:
+        # start requests session
+        with requests.Session() as s:
+            # get the response object
+            print("step 1: ")
+            response = s.get(URL, headers=header, proxies=proxy_list[proxy])
+            print("response:", response.status_code)
+
+            # get key from cookies
+            key = response.cookies['HoldTheDoor']
+
+            # compose data that will send
+            payload = {
+                'id': '1',
+                'key': key,
+                'holdthedoor': 'submit'
+            }
+
+            # send data
+            print("\nstep 2: ")
+            send = s.post(URL, headers=header, proxies=proxy_list[proxy], data=payload)
+            print("send:", send.status_code)
+    except Exception as err:
+        print(err)
--- level_4/proxies.py
+++ level_4/proxies.py
@@ -1,7 +1,26 @@
+from lxml.html import fromstring
 import requests
-from bs4 import BeautifulSoup
 
-s = requests.get('https://www.freeproxylists.net/')
-print(s.text, type(s.text))
-# soup = BeautifulSoup(s.text, 'html.parser')
-# print(soup.xpath('body'))
+######################FIND PROXIES#########################################
+def get_proxies():
+    url = 'https://free-proxy-list.net/'
+    response = requests.get(url)
+    parser = fromstring(response.text)
+    proxies = set()
+    for i in parser.xpath('//tbody/tr')[:299]:   #299 proxies max
+        proxy = ":".join([i.xpath('.//td[1]/text()')[0],i.xpath('.//td[2]/text()')[0]])
+        proxies.add('http://{}'.format(proxy))
+    return proxies
+
+
+
+######################write to file in format   IP:PORT######################
+try:
+    proxies = get_proxies()
+    f=open('proxy_list.txt','w')
+    for proxy in proxies:
+        f.write(proxy+'\n')
+    f.close()
+    print ("DONE")
+except:
+    print ("MAJOR ERROR")
